40f76c63df46f2d7354c46a0c51bfc733d7bfd9c
utokusa <utokusa@gmail.com>
2019-12-31 23:50:42 +0900

    Updates

---
+++ precache-manifest.e731ba05c988c4944e4263190fe16bbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf443ee847b61d4146ab3b0e0e927f12",
+    "url": "/hex_bookmark/index.html"
+  },
+  {
+    "revision": "1181331a8b81b6156df2",
+    "url": "/hex_bookmark/static/js/2.b60f50e7.chunk.js"
+  },
+  {
+    "revision": "be052cff3a4058ab90b2",
+    "url": "/hex_bookmark/static/js/main.1ecb58a8.chunk.js"
+  },
+  {
+    "revision": "4e534961dcd7cf2e46a7",
+    "url": "/hex_bookmark/static/js/runtime-main.dc05da4d.js"
+  },
+  {
+    "revision": "5b607bd49bbaf913342a6c50f90a288e",
+    "url": "/hex_bookmark/static/media/logo.5b607bd4.svg"
+  },
+  {
+    "revision": "cbf2fd300c133061e2f420a66e8d53b5",
+    "url": "/hex_bookmark/static/media/shape.cbf2fd30.svg"
+  }
+]);
